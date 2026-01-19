55d072176c58cf5ce64dda02e170936541dda5e2
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-19 11:46:20 +0100

    added authstate navigation to mainActivity

--- app/src/main/java/com/example/commitorquitapp/MainActivity.kt
+++ app/src/main/java/com/example/commitorquitapp/MainActivity.kt
@@ -3,26 +3,35 @@ package com.example.commitorquitapp
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.commitorquitapp.auth.AuthState
+import com.example.commitorquitapp.auth.AuthViewModel
 import com.example.commitorquitapp.databinding.ActivityMainBinding
 import com.example.commitorquitapp.ui.CreateGoalFragment
 import com.example.commitorquitapp.ui.NotificationFragment
+import com.example.commitorquitapp.ui.navigation.AppNavigator
+import com.example.commitorquitapp.ui.navigation.Navigator
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navigator: Navigator
+    private lateinit var authViewModel: AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+        navigator = AppNavigator(this)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Setup bottom nav
+
         binding.bottomNav.setupWithNavController(navController)
 
         binding.bottomNav.menu
@@ -37,6 +46,20 @@ class MainActivity : AppCompatActivity() {
         binding.btnNotification.setOnClickListener {
             val dialog = NotificationFragment()
             dialog.show(supportFragmentManager, "notifications_fragment_dialog")
+        }
+
+        authViewModel.authState.observe(this) {state ->
+            when (state) {
+                AuthState.LoggedIn -> {
+
+                }
+                AuthState.NeedsOnboarding -> {
+                    navigator.toOnboarding()
+                }
+
+                AuthState.LoggedOut ->
+                    navigator.toLogin()
+            }
         }
     }
 
