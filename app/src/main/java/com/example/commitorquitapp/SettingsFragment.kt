3bcfb0a18f4289c421d5fa7134e84acc23b7fbd6
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-01-19 13:19:34 +0100

    initial settings fragment was implemented

---
+++ app/src/main/java/com/example/commitorquitapp/SettingsFragment.kt
@@ -0,0 +1,69 @@
+package com.example.commitorquitapp
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.commitorquitapp.auth.AuthState
+import com.example.commitorquitapp.auth.AuthViewModel
+import com.example.commitorquitapp.databinding.FragmentSettingsBinding
+import com.example.commitorquitapp.ui.navigation.AppNavigator
+import com.example.commitorquitapp.ui.navigation.Navigator
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class SettingsFragment : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    private lateinit var authViewModel: AuthViewModel
+    private lateinit var navigator: Navigator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+        navigator = AppNavigator(requireActivity())
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            authViewModel.onLogout()
+        }
+
+        binding.btnBack.setOnClickListener {
+            dismiss()
+        }
+
+
+
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        dialog?.window?.setWindowAnimations(R.style.DialogSlideInLeftAnimation)
+
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+
+}
